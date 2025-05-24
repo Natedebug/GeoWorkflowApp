3b22addba0c71fd4fbc58feaac9edac46b4dd681
Nathan <nathanhathorn2916@yahoo.com>
2025-05-23 22:49:24 -0500

    Add Xcode starter macOS app

---
+++ GeoWorkflowApp/GeoWorkflowApp/GeoWorkflowAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeoWorkflowAppApp.swift
+//  GeoWorkflowApp
+//
+//  Created by Nathan Hathorn on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct GeoWorkflowAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
